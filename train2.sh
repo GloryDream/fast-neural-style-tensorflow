27e616801147e303bc1f93f1a6d491dc5ecf0833
GloryDream <xy_Gong@foxmail.com>
2018-03-07 11:27:02 +0800

    Update.

--- train2.sh
+++ train2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export CUDA_VISIBLE_DEVICES=6
-#python pretrain.py -c conf/cubist.yml
-#python pretrain.py -c conf/scream.yml
+python pretrain.py -c conf/cubist.yml
+python pretrain.py -c conf/scream.yml
 python pretrain.py -c conf/candy.yml
 python pretrain.py -c conf/delaunay.yml
 python pretrain.py -c conf/comp.yml
--- train3.sh
+++ train3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-export CUDA_VISIBLE_DEVICES=7
-#python pretrain.py -c conf/feathers.yml
-#python pretrain.py -c conf/starry.yml
+export CUDA_VISIBLE_DEVICES=5
+python pretrain.py -c conf/feathers.yml
+python pretrain.py -c conf/starry.yml
 python pretrain.py -c conf/picasso.yml
 python pretrain.py -c conf/udnie.yml
 python pretrain.py -c conf/gothic.yml
